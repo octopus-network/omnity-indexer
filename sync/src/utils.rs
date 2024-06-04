32115384e8c1051a5963136ca77222a7a9f42eaa
Ting A Lin <linshaoting6@gmail.com>
2024-06-04 11:23:13 +0700

    remove unused functions

--- sync/src/utils.rs
+++ sync/src/utils.rs
@@ -1,14 +1,12 @@
 use config::{Config, ConfigError};
 
 use ic_agent::agent::http_transport::ReqwestTransport;
-use ic_agent::identity::{Prime256v1Identity, Secp256k1Identity};
-use ic_agent::{export::Principal, identity::BasicIdentity, Agent, Identity};
+use ic_agent::identity::Secp256k1Identity;
+use ic_agent::{export::Principal, Agent, Identity};
 use ic_btc_interface::Txid;
-use ic_identity_hsm::HardwareIdentity;
 
 use lazy_static::lazy_static;
 use log::{debug, info};
-use ring::signature::Ed25519KeyPair;
 use sea_orm::ConnectOptions;
 use sea_orm::DatabaseConnection;
 use serde::Deserialize;
@@ -17,94 +15,6 @@ use anyhow::{Error as AnyError, Result};
 use std::sync::{Arc, RwLock};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{error::Error, future::Future, path::Path};
-
-const HSM_PKCS11_LIBRARY_PATH: &str = "HSM_PKCS11_LIBRARY_PATH";
-const HSM_SLOT_INDEX: &str = "HSM_SLOT_INDEX";
-const HSM_KEY_ID: &str = "HSM_KEY_ID";
-const HSM_PIN: &str = "HSM_PIN";
-// const LOCAL_NET: &str = "http://127.0.0.1:4943";
-
-pub fn get_effective_canister_id() -> Principal {
-	Principal::from_text("rwlgt-iiaaa-aaaaa-aaaaa-cai").unwrap()
-}
-
-pub fn create_identity() -> Result<Box<dyn Identity>, String> {
-	if std::env::var(HSM_PKCS11_LIBRARY_PATH).is_ok() {
-		create_hsm_identity().map(|x| Box::new(x) as _)
-	} else {
-		create_basic_identity().map(|x| Box::new(x) as _)
-	}
-}
-
-fn expect_env_var(name: &str) -> Result<String, String> {
-	std::env::var(name).map_err(|_| format!("Need to specify the {} environment variable", name))
-}
-
-pub fn create_hsm_identity() -> Result<HardwareIdentity, String> {
-	let path = expect_env_var(HSM_PKCS11_LIBRARY_PATH)?;
-	let slot_index = expect_env_var(HSM_SLOT_INDEX)?
-		.parse::<usize>()
-		.map_err(|e| format!("Unable to parse {} value: {}", HSM_SLOT_INDEX, e))?;
-	let key = expect_env_var(HSM_KEY_ID)?;
-	let id = HardwareIdentity::new(path, slot_index, &key, get_hsm_pin)
-		.map_err(|e| format!("Unable to create hw identity: {}", e))?;
-	Ok(id)
-}
-
-fn get_hsm_pin() -> Result<String, String> {
-	expect_env_var(HSM_PIN)
-}
-
-// The SoftHSM library doesn't like to have two contexts created/initialized at once.
-// Trying to create two HardwareIdentity instances at the same time results in this error:
-//    Unable to create hw identity: PKCS#11: CKR_CRYPTOKI_ALREADY_INITIALIZED (0x191)
-//
-// To avoid this, we use a basic identity for any second identity in tests.
-//
-// A shared container of Ctx objects might be possible instead, but my rust-fu is inadequate.
-pub fn create_basic_identity() -> Result<BasicIdentity, String> {
-	let rng = ring::rand::SystemRandom::new();
-	let key_pair = ring::signature::Ed25519KeyPair::generate_pkcs8(&rng)
-		.expect("Could not generate a key pair.");
-
-	Ok(BasicIdentity::from_key_pair(
-		Ed25519KeyPair::from_pkcs8(key_pair.as_ref()).expect("Could not read the key pair."),
-	))
-}
-
-/// Create a secp256k1identity, which unfortunately will always be the same one
-/// (So can only use one per test)
-pub fn create_secp256k1_identity() -> Result<Secp256k1Identity, String> {
-	// generated from the the following commands:
-	// $ openssl ecparam -name secp256k1 -genkey -noout -out identity.pem
-	// $ cat identity.pem
-	let identity_file = "
------BEGIN EC PRIVATE KEY-----
-MHQCAQEEIJb2C89BvmJERgnT/vJLKpdHZb/hqTiC8EY2QtBRWZScoAcGBSuBBAAK
-oUQDQgAEDMl7g3vGKLsiLDA3fBRxDE9ZkM3GezZFa5HlKM/gYzNZfU3w8Tijjd73
-yeMC60IsMNxDjLqElV7+T7dkb5Ki7Q==
------END EC PRIVATE KEY-----";
-
-	let identity = Secp256k1Identity::from_pem(identity_file.as_bytes())
-		.expect("Cannot create secp256k1 identity from PEM file.");
-	Ok(identity)
-}
-
-pub fn create_prime256v1_identity() -> Result<Prime256v1Identity, String> {
-	// generated from the following command:
-	// $ openssl ecparam -name prime256v1 -genkey -noout -out identity.pem
-	// $ cat identity.pem
-	let identity_file = "\
------BEGIN EC PRIVATE KEY-----
-MHcCAQEEIL1ybmbwx+uKYsscOZcv71MmKhrNqfPP0ke1unET5AY4oAoGCCqGSM49
-AwEHoUQDQgAEUbbZV4NerZTPWfbQ749/GNLu8TaH8BUS/I7/+ipsu+MPywfnBFIZ
-Sks4xGbA/ZbazsrMl4v446U5UIVxCGGaKw==
------END EC PRIVATE KEY-----";
-
-	let identity = Prime256v1Identity::from_pem(identity_file.as_bytes())
-		.expect("Cannot create prime256v1 identity from PEM file.");
-	Ok(identity)
-}
 
 pub async fn create_agent(identity: impl Identity + 'static) -> Result<Agent, String> {
 	// let network = std::env::var("DFX_NETWORK").unwrap_or_else(|_| LOCAL_NET.to_string());
