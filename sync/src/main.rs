fa96b7816008aece43b7e101f0c35e4a639930c9
Ting A Lin <linshaoting6@gmail.com>
2024-06-19 15:31:13 +0700

    add env

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -1,36 +1,41 @@
+use anyhow::anyhow;
 use clap::{Parser, Subcommand};
+use dotenvy::dotenv;
 use omnity_indexer_sync::{tasks::execute_sync_tasks, utils::*};
-
 use std::env;
 
-// #[cfg(debug_assertions)]
-// use dotenvy::dotenv;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-	/// the path to the config file
 	#[arg(short, long)]
 	config: Option<String>,
 
 	#[command(subcommand)]
 	command: Option<Commands>,
 }
+
 #[derive(Subcommand, Debug)]
 enum Commands {
-	/// start sync tasks
-	Start,
+	/// Use config
+	Config,
+	/// Use env
+	Env,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// #[cfg(debug_assertions)]
-	// dotenv().ok();
+	dotenv().ok();
 
 	let cli = Cli::parse();
-	let exe_path = env::current_exe()?;
-	let exe_dir = exe_path.parent().unwrap();
-	// println!("exe_dir: {:?}", exe_dir);
 
+	let exe_path = env::current_exe()?;
+	let exe_dir = exe_path
+		.parent()
+		.unwrap()
+		.parent()
+		.unwrap()
+		.parent()
+		.unwrap();
 	let default_config_filename = "config.toml";
 	let default_config_path = exe_dir.join(default_config_filename);
 
@@ -40,9 +45,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		.unwrap_or_else(|| default_config_path.to_str().unwrap().to_string());
 
 	let settings = Settings::new(&config_path)?;
-	// println!("settings: {:?}", settings);
+
 	set_config(settings);
-	// init log
+
+	// Initial log
 	let log_config = read_config(|c| c.log_config.to_owned());
 	if let Err(e) = log4rs::init_file(log_config, Default::default()) {
 		eprintln!("init log failed: {}", e);
@@ -50,9 +56,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	}
 
 	match &cli.command {
-		Some(Commands::Start) => {
+		Some(Commands::Config) => {
 			// init database
 			let db_url = read_config(|c| c.database_url.to_owned());
+			let db = Database::new(db_url).await;
+
+			execute_sync_tasks(db.get_connection()).await;
+		}
+		Some(Commands::Env) => {
+			let db_url =
+				std::env::var("DATABASE_URL").map_err(|_| anyhow!("DATABASE_URL is not found"))?;
 			let db = Database::new(db_url).await;
 
 			execute_sync_tasks(db.get_connection()).await;
