35fa28a3bb4a58996a5412f63cae2be01c3eeca2
Ting A Lin <linshaoting6@gmail.com>
2025-01-07 13:02:37 +0700

    comments

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -1,26 +1,26 @@
 use anyhow::anyhow;
 use dotenvy::dotenv;
-// use log::LevelFilter;
-// use log4rs::{
-// 	append::console::ConsoleAppender,
-// 	config::{Appender, Root},
-// };
+use log::LevelFilter;
+use log4rs::{
+	append::console::ConsoleAppender,
+	config::{Appender, Root},
+};
 use omnity_indexer_sync::{tasks::execute_sync_tasks, utils::*};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	dotenv().ok();
-	// let stdout = ConsoleAppender::builder().build();
-	// let config = log4rs::config::Config::builder()
-	// 	.appender(Appender::builder().build("stdout", Box::new(stdout)))
-	// 	.build(Root::builder().appender("stdout").build(LevelFilter::Info))
-	// 	.unwrap();
-	// log4rs::init_config(config).unwrap();
+	let stdout = ConsoleAppender::builder().build();
+	let config = log4rs::config::Config::builder()
+		.appender(Appender::builder().build("stdout", Box::new(stdout)))
+		.build(Root::builder().appender("stdout").build(LevelFilter::Info))
+		.unwrap();
+	log4rs::init_config(config).unwrap();
 
-	if let Err(e) = log4rs::init_file("./log4rs.yaml", Default::default()) {
-		eprintln!("init log failed: {}", e);
-		std::process::exit(1);
-	}
+	// if let Err(e) = log4rs::init_file("./log4rs.yaml", Default::default()) {
+	// 	eprintln!("init log failed: {}", e);
+	// 	std::process::exit(1);
+	// }
 
 	let db_url = std::env::var("DATABASE_URL").map_err(|_| anyhow!("DATABASE_URL is not found"))?;
 	let db = Database::new(db_url.clone()).await;
