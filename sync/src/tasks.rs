75908adb8fb6f58fee55a7bbd88c076c6e4a51fa
Ting A Lin <linshaoting6@gmail.com>
2025-06-30 18:45:49 +0200

    tasks

--- sync/src/tasks.rs
+++ sync/src/tasks.rs
@@ -35,7 +35,7 @@ where
 }
 
 pub async fn execute_sync_tasks(db_conn: Arc<DbConn>) {
-	let remove_database = async {
+	let _remove_database = async {
 		// let _ = Delete::remove_chains(&db_conn).await;
 		// let _ = Delete::remove_tokens(&db_conn).await;
 		// let _ = Delete::remove_tickets(&db_conn).await;
@@ -63,16 +63,34 @@ pub async fn execute_sync_tasks(db_conn: Arc<DbConn>) {
 		|db_conn| async move { hub::sync_tickets(&db_conn).await },
 	);
 
-	let sync_all_token_ledger_id_on_chain = spawn_sync_task(
+	let sync_all_token_ledger_id_on_chain_from_icp = spawn_sync_task(
 		db_conn.clone(),
 		TOKEN_LEDGER_ID_ON_CHAIN_SYNC_INTERVAL,
-		|db_conn| async move {
-			icp::sync_all_icp_token_ledger_id_on_chain(&db_conn).await?;
-			evm::sync_all_token_ledger_id_from_evm_route(&db_conn).await?;
-			sicp::sync_all_icrc_token_canister_id_from_sicp(&db_conn).await?;
-			cosmwasm::sync_all_cosmwasm_token_ledger_id_on_chain(&db_conn).await?;
-			ton::sync_all_ton_token_ledger_id_on_chain(&db_conn).await
-		},
+		|db_conn| async move { icp::sync_all_icp_token_ledger_id_on_chain(&db_conn).await },
+	);
+
+	let sync_all_token_ledger_id_from_evm = spawn_sync_task(
+		db_conn.clone(),
+		TOKEN_LEDGER_ID_ON_CHAIN_SYNC_INTERVAL,
+		|db_conn| async move { evm::sync_all_token_ledger_id_from_evm_route(&db_conn).await },
+	);
+
+	let sync_all_token_canister_id_from_sicp = spawn_sync_task(
+		db_conn.clone(),
+		TOKEN_LEDGER_ID_ON_CHAIN_SYNC_INTERVAL,
+		|db_conn| async move { sicp::sync_all_icrc_token_canister_id_from_sicp(&db_conn).await },
+	);
+
+	let sync_all_token_ledger_id_from_cosmwasm = spawn_sync_task(
+		db_conn.clone(),
+		TOKEN_LEDGER_ID_ON_CHAIN_SYNC_INTERVAL,
+		|db_conn| async move { cosmwasm::sync_all_cosmwasm_token_ledger_id_on_chain(&db_conn).await },
+	);
+
+	let sync_all_token_ledger_id_from_ton = spawn_sync_task(
+		db_conn.clone(),
+		TOKEN_LEDGER_ID_ON_CHAIN_SYNC_INTERVAL,
+		|db_conn| async move { ton::sync_all_ton_token_ledger_id_on_chain(&db_conn).await },
 	);
 
 	let sync_tokens_on_chains_from_hub = spawn_sync_task(
@@ -81,30 +99,76 @@ pub async fn execute_sync_tasks(db_conn: Arc<DbConn>) {
 		|db_conn| async move { hub::sync_tokens_on_chains(&db_conn).await },
 	);
 
-	let sync_ticket_status = spawn_sync_task(
+	let sync_ticket_status_from_sui = spawn_sync_task(
 		db_conn.clone(),
 		TICKET_SYNC_INTERVAL,
-		|db_conn| async move {
-			sui::sync_ticket_status_from_sui(&db_conn).await?;
-			doge::sync_ticket_status_from_doge(&db_conn).await?;
-			solana::sync_ticket_status_from_solana_route(&db_conn).await?;
-			solana_custom::sync_ticket_status_from_solana_custom(&db_conn).await?;
-			bitcoin::sync_all_ticket_status_from_bitcoin(&db_conn).await?;
-			sicp::sync_ticket_status_from_sicp(&db_conn).await?;
-			icp::sync_ticket_status_from_icp_route(&db_conn).await?;
-			evm::sync_all_tickets_status_from_evm_route(&db_conn).await?;
-			cosmwasm::sync_all_tickets_status_from_cosmwasm_route(&db_conn).await?;
-			ton::sync_all_tickets_status_from_ton_route(&db_conn).await
-		},
+		|db_conn| async move { sui::sync_ticket_status_from_sui(&db_conn).await },
 	);
 
-	let update_sender_mint_tickets = spawn_sync_task(
+	let sync_ticket_status_from_doge = spawn_sync_task(
 		db_conn.clone(),
 		TICKET_SYNC_INTERVAL,
-		|db_conn| async move {
-			hub::update_sender(&db_conn).await?;
-			bitcoin::update_mint_tickets(&db_conn).await
-		},
+		|db_conn| async move { doge::sync_ticket_status_from_doge(&db_conn).await },
+	);
+
+	let sync_ticket_status_from_solana_route = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { solana::sync_ticket_status_from_solana_route(&db_conn).await },
+	);
+
+	let sync_ticket_status_from_solana_custom = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { solana_custom::sync_ticket_status_from_solana_custom(&db_conn).await },
+	);
+
+	let sync_ticket_status_from_bitcoin = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { bitcoin::sync_all_ticket_status_from_bitcoin(&db_conn).await },
+	);
+
+	let sync_ticket_status_from_sicp = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { sicp::sync_ticket_status_from_sicp(&db_conn).await },
+	);
+
+	let sync_ticket_status_from_eicp = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { icp::sync_ticket_status_from_icp_route(&db_conn).await },
+	);
+
+	let sync_all_tickets_status_from_evm = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { evm::sync_all_tickets_status_from_evm_route(&db_conn).await },
+	);
+
+	let sync_all_tickets_status_from_cosmwasm = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { cosmwasm::sync_all_tickets_status_from_cosmwasm_route(&db_conn).await },
+	);
+
+	let sync_all_tickets_status_from_ton = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { ton::sync_all_tickets_status_from_ton_route(&db_conn).await },
+	);
+
+	let update_sender_tickets_from_hub = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { hub::update_sender(&db_conn).await },
+	);
+
+	let update_mint_tickets_from_btc = spawn_sync_task(
+		db_conn.clone(),
+		TICKET_SYNC_INTERVAL,
+		|db_conn| async move { bitcoin::update_mint_tickets(&db_conn).await },
 	);
 
 	let update_deleted_mint_tickets_from_btc = spawn_sync_task(
@@ -126,14 +190,28 @@ pub async fn execute_sync_tasks(db_conn: Arc<DbConn>) {
 	);
 
 	let _ = tokio::join!(
-		remove_database,
+		// remove_database,
 		sync_chains_task,
 		sync_tokens_task,
 		sync_tickets_task,
-		sync_all_token_ledger_id_on_chain,
+		sync_all_token_ledger_id_on_chain_from_icp,
+		sync_all_token_ledger_id_from_evm,
+		sync_all_token_canister_id_from_sicp,
+		sync_all_token_ledger_id_from_cosmwasm,
+		sync_all_token_ledger_id_from_ton,
 		sync_tokens_on_chains_from_hub,
-		sync_ticket_status,
-		update_sender_mint_tickets,
+		sync_ticket_status_from_sui,
+		sync_ticket_status_from_doge,
+		sync_ticket_status_from_solana_route,
+		sync_ticket_status_from_solana_custom,
+		sync_ticket_status_from_bitcoin,
+		sync_ticket_status_from_sicp,
+		sync_ticket_status_from_eicp,
+		sync_all_tickets_status_from_evm,
+		sync_all_tickets_status_from_cosmwasm,
+		sync_all_tickets_status_from_ton,
+		update_sender_tickets_from_hub,
+		update_mint_tickets_from_btc,
 		update_deleted_mint_tickets_from_btc,
 		update_total_volumes_from_hub,
 		update_sync_bridge_fee_log_hub,
